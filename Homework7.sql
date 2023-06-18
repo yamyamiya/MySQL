129977405f916158517dc85abcd54d805e7952fd
OLGA IAMSHCHIKOVA <54511031+yamyamiya@users.noreply.github.com>
2023-06-18 19:29:04 +0200

    Add files via upload

--- Homework7.sql
+++ Homework7.sql
@@ -1,64 +1,62 @@
-create database homework7;
-use homework7;
+CREATE DATABASE homework7;
+USE homework7;
 
 -- 1) вывести название продукта, который чаще всего встречается в заказах 
-select 
+SELECT
 	title
-from products
-where id=(
-select 
+FROM products
+WHERE id=(
+	SELECT 
 		productid
-	from orders
-    group by productid
-    having count(*) = (
-select 
-		max(count) as max
-        from(
-select count(*) AS count,
-productid
-from orders
-group by productid
-Order by productid
-)t1
-)
+	FROM orders
+    GROUP BY productid
+    HAVING COUNT(*) = (
+			SELECT 
+				MAX(count) AS max
+			FROM(
+				SELECT COUNT(*) AS count,
+					productid
+				FROM orders
+				GROUP BY productid
+				)t1
+	)
 );
 
 -- 2) вывести название продукта, который покупают чаще всего (нужно посчетать общее количество купленных единиц)
-select 
+SELECT 
 	title
-from products
-where id=(
-select 
+FROM products
+WHERE id=(
+	SELECT 
 		productid
-	from orders
-    group by productid
-    having sum(productcount) = (
-select 
-		max(sum) as max
-        from(
-select sum(productcount) AS sum,
-productid
-from orders
-group by productid
-Order by productid
-)t1
-)
+	FROM orders
+    GROUP BY productid
+    HAVING SUM(productcount) = (
+		SELECT 
+			MAX(sum) AS max
+        FROM(
+			SELECT SUM(productcount) AS sum,
+				productid
+			FROM orders
+			GROUP BY productid
+			)t1
+	)
 );
 
 -- 3) вывести общую выручку магазина. Сумма всех купленных единиц товара.
-select 
-sum(t1.price*t2.productcount) as total_money
-from products t1
-inner join orders t2
-on t1.id = t2.productid;
+SELECT 
+	SUM(t1.price*t2.productcount) AS total_money
+FROM products t1
+INNER JOIN orders t2
+ON t1.id = t2.productid;
 
 -- 4) определить сумму выручки за онлайн продажи (online) и за продажи в магазине (direct) 
-select 
-sum(t1.price*t2.productcount) as total_money
-from products t1
-inner join orders t2
-on t1.id = t2.productid
-group by t2.ordertype;
+SELECT 
+	SUM(t1.price*t2.productcount) AS total_money
+FROM products t1
+INNER JOIN orders t2
+ON t1.id = t2.productid
+GROUP BY t2.ordertype;
  
 
 
